83e14c4104831df8292300d40dba7c18490efe8c
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-01-15 11:38:20 -0500

    first commit

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,82 @@
-// Write your "actions" router here!
+const express = require("express");
+const router = express.Router();
+
+const Action = require("./actions-model");
+
+router.get("/", (req, res) => {
+  Action.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Could not be retrieved" });
+    });
+});
+
+router.get("/:id", validateActionId, (req, res) => {
+  res.status(200).json(req.action);
+});
+
+router.post("/", validateAction, (req, res) => {
+  Action.insert(req.body)
+    .then((action) => {
+      res.status(201).json(action);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Error creating action" });
+    });
+});
+
+router.put("/:id", [validateActionId, validateAction], (req, res) => {
+  Action.update(req.params.id, req.body)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Error updating action" });
+    });
+});
+
+router.delete("/:id", validateActionId, (req, res) => {
+  Action.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "Action is deleted" });
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Error removing action" });
+    });
+});
+
+// Middleware
+
+function validateActionId(req, res, next) {
+  Action.get(req.params.id)
+    .then((action) => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        res.status(404).json({ message: "invalid action id" });
+      }
+    })
+    .catch((error) => {
+      console.log(error.message);
+      req.status(500).json({ message: "Error retrieving action" });
+    });
+}
+
+function validateAction(req, res, next) {
+  if (!req.body.description || !req.body.project_id || !req.body.notes) {
+    res
+      .status(400)
+      .json({ message: "Please provide description, project id, and notes" });
+  } else {
+    next();
+  }
+}
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,94 @@
-// Write your "projects" router here!
+const express = require("express");
+const router = express.Router();
+
+const Project = require("./projects-model");
+
+router.get("/", (req, res) => {
+  Project.get()
+    .then((projects) => {
+      console.log(projects);
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Projects could not be retrieved" });
+    });
+});
+
+router.get("/:id", validateProjectId, (req, res) => {
+  res.status(200).json(req.project);
+});
+
+router.get("/:id/actions", validateProjectId, (req, res) => {
+  Project.getProjectActions(req.params.id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res
+        .status(500)
+        .json({ message: "Project actions could not be retrieved" });
+    });
+});
+
+router.post("/", validateProject, (req, res) => {
+  Project.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Error creating project" });
+    });
+});
+
+router.put("/:id", [validateProjectId, validateProject], (req, res) => {
+  Project.update(req.params.id, req.body)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Error updating project" });
+    });
+});
+
+router.delete("/:id", validateProjectId, (req, res) => {
+  Project.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "The project is removed" });
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Error removing project" });
+    });
+});
+
+// Middleware
+
+function validateProjectId(req, res, next) {
+  Project.get(req.params.id)
+    .then((project) => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        res.status(404).json({ message: "invalid project id" });
+      }
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: "Error retrieving project" });
+    });
+}
+
+function validateProject(req, res, next) {
+  if (!req.body.name || !req.body.description) {
+    res.status(400).json({ message: "Please provide name and description" });
+  } else {
+    next();
+  }
+}
+
+module.exports = router;
